47df5fb2959fa52c3e8c87fe4002ad68efdce9d4
M4TH3US17 <matheusdalvino50@gmail.com>
2023-07-21 15:52:36 -0400

    Hover fixo

--- src/componentes/NavBar/MenuDesktop/menudesktop.tsx
+++ src/componentes/NavBar/MenuDesktop/menudesktop.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import './menudesktop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 interface MenuDesktopProps {
     handleClickOnSair: any;
 }
 
 const MenuDesktop: React.FC<MenuDesktopProps> = ({ handleClickOnSair }) => {
+    const location = useLocation();
 
     return (
         <div className='menu-desktop'>
-            <Link to={"/"} className='btn  btn-desktop'>Home</Link>
-            <Link to={"/administracao"} className='btn btn-desktop'>Admin</Link>
+            <Link to={"/"} className={location.pathname === "/" ? "isSelected btn btn-desktop" : "btn btn-desktop"}>Home</Link>
+            <Link to={"/administracao"} className={location.pathname === "/administracao" ? "isSelected btn btn-desktop" : "btn btn-desktop"}>Admin</Link>
             <button className='btn btn-desktop' onClick={handleClickOnSair}>Sair</button>
         </div>
     );
